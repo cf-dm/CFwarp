0ec25ba4968829385c14ede63eb0722e57761eae
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2024-01-18 05:43:06 +0000

    Update CFcdnym.sh

--- point/CFcdnym.sh
+++ point/CFcdnym.sh
@@ -13,9 +13,8 @@ elif [ -x "$(command -v dnf)" ]; then
 dnf update -y >/dev/null 2>&1
 dnf install lzip unzip qrencode -y >/dev/null 2>&1
 fi
-pkg update
-#pkg upgrade -y
-pkg install curl wget lzip
+pkg upgrade -y
+pkg install curl wget lzip -y
 apk add openssh curl wget lzip
 touch yg_update
 fi
